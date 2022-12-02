4dbf6dd4c9bad8a8bdf23da1d79957f18e9b1226
Daniel Volski <danfvdaum3@hotmail.com>
2022-12-02 10:45:19 -0300

    Added some tests to the insert and search functions

--- main.c
+++ main.c
@@ -3,5 +3,25 @@
 
 int main()
 {
+    tree_node *root = init_binary_tree(root);
+    root = insert_in_binary_tree(root, 2);
+    root = insert_in_binary_tree(root, 1);
+    root = insert_in_binary_tree(root, 3);
 
+    // Deve imprimir 2
+    printf("%d\n", root->value);
+    // Deve imprimir 1
+    printf("%d\n", root->left->value);
+    // Deve imprimir 3
+    printf("%d\n", root->right->value);
+
+    // Imprime os endereços dos nós da árvore
+    printf("%p\n", root);
+    printf("%p\n", root->left);
+    printf("%p\n", root->right);
+
+    // Deve imprimir os mesmos endereços listados acima
+    printf("%p\n", search_in_binary_tree(root, 2));
+    printf("%p\n", search_in_binary_tree(root, 1));
+    printf("%p\n", search_in_binary_tree(root, 3));
 }
