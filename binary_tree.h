2c47be01a3c263b1c7d18731669b0e847d0480ab
Daniel Volski <danfvdaum3@hotmail.com>
2022-12-02 10:45:44 -0300

    Added the search function

--- binary_tree.h
+++ binary_tree.h
@@ -42,3 +42,25 @@ tree_node* insert_in_binary_tree(tree_node *node, int value)
     }
     return node;
 }
+
+// Faz a busca pelo valor solicitado a partir do nó raiz da árvore
+tree_node* search_in_binary_tree(tree_node *root, int value)
+{
+    if (root == NULL)
+        return NULL;
+    else
+    {
+        tree_node *aux_node = (tree_node*) malloc(sizeof(tree_node));
+        aux_node = root;
+        while (aux_node->value != value)
+        {
+            if (aux_node->value < value && aux_node->right != NULL)
+                aux_node = aux_node->right;
+            else if (aux_node->value > value && aux_node->left != NULL)
+                aux_node = aux_node->left;
+            else
+                break;
+        }
+        return aux_node;
+    }
+}
